ef3b4083877283422bdb76eef2ca4d8d3c31e4bc
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-10-02 12:34:50 +0200

    Hetedik

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -25,6 +25,7 @@ namespace gyak4
         public void LoadData()
         {
             Flats = context.Flats.ToList();
+
         }
         public void CreateExcel()
         {
@@ -72,6 +73,37 @@ namespace gyak4
                 xlSheet.Cells[1, i + 1] = headers[i];
 
             }
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+            }
+            xlSheet.get_Range(GetCell(2,1), GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
         public Form1()

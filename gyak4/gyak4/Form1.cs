bc2a51617d5e8d6e130cde1355828f497b2a398b
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-10-02 11:16:03 +0200

    Harmadik

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -12,9 +12,16 @@ namespace gyak4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        public void LoadData()
+        {
+
+        }
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
     }
 }

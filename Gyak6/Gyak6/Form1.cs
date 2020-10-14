224d17749587ffef0fd4d52bfd95470a9d529e79
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-10-14 13:13:15 +0200

    Chart

--- Gyak6/Gyak6/Form1.cs
+++ Gyak6/Gyak6/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace Gyak6
@@ -55,11 +56,30 @@ namespace Gyak6
 
             }
         }
+        private void Chart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
         public Form1()
         {
             InitializeComponent();
             Fuggveny();
             dataGridView1.DataSource = Rates;
+
         }
     }
 }

b6603815af60f60b10f5fad260fc2d88e33bbdd8
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-11-16 11:13:32 +0100

    11

--- gyak8/gyak8/Form1.cs
+++ gyak8/gyak8/Form1.cs
@@ -14,13 +14,30 @@ namespace gyak8
 {
     public partial class Form1 : Form
     {
+        
         List<Toy> _toys = new List<Toy>();
+        private Toy _nextToy;
+        private void DisplayNext()
+        {
+            if (_nextToy!=null)
+            {
+                Controls.Remove(_nextToy);
+                _nextToy = Factory.CreateNew();
+                _nextToy.Top = label1.Top + label1.Height + 20;
+                _nextToy.Left = label1.Left;
+                Controls.Add(_nextToy);
+            }
+        }
         private IToyFactory _factory;
 
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            { 
+                _factory = value;
+                DisplayNext();
+            }
         }
 
 
@@ -59,6 +76,16 @@ namespace gyak8
 
             }
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
         }
     }
 }

04fa7572193370ac109ffa2433ff1469f8d8299a
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-11-19 17:19:46 +0100

    10

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fluent.Infrastructure.FluentController;
 using NUnit.Framework;
 using UnitTestExample;
 
@@ -11,6 +12,7 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
+        
         [Test,
         TestCase("abcd1234", false),
         TestCase("irf@uni-corvinus", false),
@@ -22,12 +24,31 @@ namespace UnitTestExample.Test
             
             var accountController = new AccountController();
 
-            
-            var actualResult = accountController.ValidateEmail(email);
+
+            var actualResult = accountController.ValidateEmail();
 
             
             Assert.AreEqual(expectedResult, actualResult);
 
+        }
+
+        [
+    Test,
+    TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            
+            var accountController = new AccountController();
+
+            
+            var actualResult = accountController.Register(email, password);
+
+            
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
         }
     }
 }

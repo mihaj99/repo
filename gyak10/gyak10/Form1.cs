2d86ccefff7215f022ee138fcc7edd5d8ec22c3e
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-11-29 15:29:57 +0100

    7

--- gyak10/gyak10/Form1.cs
+++ gyak10/gyak10/Form1.cs
@@ -19,6 +19,7 @@ namespace gyak10
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        Brain winnerBrain = null;
 
         public Form1()
         {
@@ -65,6 +66,21 @@ namespace gyak10
                     gc.AddPlayer(b.Mutate());
             }
             gc.Start();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
         }
+        
+
+        
+
+        
     }
 }

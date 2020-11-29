e4b3bded195636c097682ec5f81d37255b5d1e4e
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-11-29 15:20:35 +0100

    4

--- gyak10/gyak10/Form1.cs
+++ gyak10/gyak10/Form1.cs
@@ -15,6 +15,10 @@ namespace gyak10
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
 
         public Form1()
         {
@@ -23,6 +27,15 @@ namespace gyak10
             this.Controls.Add(ga);
             gc.AddPlayer();
             gc.Start(true);
+
+            
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
         }
     }
 }

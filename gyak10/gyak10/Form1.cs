55add00c93c611cc4e3797e243272baf869ed920
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-11-29 15:25:32 +0100

    6

--- gyak10/gyak10/Form1.cs
+++ gyak10/gyak10/Form1.cs
@@ -28,7 +28,7 @@ namespace gyak10
             gc.AddPlayer();
             gc.Start(true);
 
-            
+            gc.GameOver += Gc_GameOver;
 
             for (int i = 0; i < populationSize; i++)
             {
@@ -36,6 +36,35 @@ namespace gyak10
             }
             gc.Start();
 
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation); ;
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
